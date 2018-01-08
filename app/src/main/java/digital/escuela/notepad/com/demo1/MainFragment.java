49d988ff38a360d5f5d1508b1a89a27b7c156940
Ernesto Martin Torres Garcia <tinto.volk22@gmail.com>
2018-01-08 18:27:03 -0500

    Agregamos un fragment Creamos el .xml Creamos la clase Java Actualizamos el Layout principal

---
+++ app/src/main/java/digital/escuela/notepad/com/demo1/MainFragment.java
@@ -0,0 +1,23 @@
+package digital.escuela.notepad.com.demo1;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by martinto on 08/01/18.
+ */
+
+public class MainFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView;
+        rootView=inflater.inflate(R.layout.fragment_layout,container,false);
+        return rootView;
+    }
+}
