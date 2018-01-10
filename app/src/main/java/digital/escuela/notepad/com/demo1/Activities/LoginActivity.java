3d9421d2c62595bfe673a504e040bbf533786bc8
Ernesto Martin Torres Garcia <tinto.volk22@gmail.com>
2018-01-09 19:11:31 -0500

    Toast

--- app/src/main/java/digital/escuela/notepad/com/demo1/Activities/LoginActivity.java
+++ app/src/main/java/digital/escuela/notepad/com/demo1/Activities/LoginActivity.java
@@ -2,14 +2,39 @@ package digital.escuela.notepad.com.demo1.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import digital.escuela.notepad.com.demo1.R;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText username,password;
+    Button login,register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        username=(EditText) findViewById(R.id.username);
+        password=(EditText) findViewById(R.id.password);
+        login=(Button) findViewById(R.id.login);
+        register=(Button) findViewById(R.id.register);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LoginActivity.this,"Le diste click",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
