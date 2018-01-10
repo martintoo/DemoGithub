14875f957815d72005ccb49144cf4ca3cd8730e9
Ernesto Martin Torres Garcia <tinto.volk22@gmail.com>
2018-01-10 02:37:22 -0500

    NavigationView, header y circle image El navigation view consta de un header.xml(layout) y un menu(menu.xml) No olvidar importar la libreria de soporte support lib v4 Solo se ve, no se desliza

--- app/src/main/java/digital/escuela/notepad/com/demo1/Activities/MainActivity.java
+++ app/src/main/java/digital/escuela/notepad/com/demo1/Activities/MainActivity.java
@@ -1,20 +1,45 @@
 package digital.escuela.notepad.com.demo1.Activities;
 
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+
 import digital.escuela.notepad.com.demo1.R;
 
 public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        toolbar=(Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        navigationView=(NavigationView) findViewById(R.id.navigationView);
+        drawerLayout=(DrawerLayout) findViewById(R.id.drawer);
+
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(item.isChecked()) item.setChecked(false);
+                else item.setChecked(true);
+                return false;
+            }
+        });
+
     }
 }
 /*
