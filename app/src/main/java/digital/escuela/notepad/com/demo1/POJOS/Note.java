a640ddd5960480175359cf4c697de79da9877aac
Ernesto Martin Torres Garcia <tinto.volk22@gmail.com>
2018-01-12 13:54:47 -0500

    Implementamos el POJO Note

--- app/src/main/java/digital/escuela/notepad/com/demo1/POJOS/Note.java
+++ app/src/main/java/digital/escuela/notepad/com/demo1/POJOS/Note.java
@@ -5,4 +5,40 @@ package digital.escuela.notepad.com.demo1.POJOS;
  */
 
 public class Note {
+    private int id;
+    private String title;
+    private String body;
+
+    public Note() {
+    }
+
+    public Note(int id, String title, String body) {
+        this.setId(id);
+        this.setTitle(title);
+        this.setBody(body);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
 }
